edad3edfeedaebb30d37db7f463bfa646f75c9f9
JAO1988 <jao1988@gmail.com>
2025-01-16 22:28:52 -0500

    Additional parameter changes for OpenRGB + VIA Hybrid modes
    Added changes to tmk_core, quantum/rgb_matrix & OpenRGB animations, builddefs, via.c/via.h, openrgb.c/openrgb.h, and Keychron's Bluetooth/Common folders for additional parameters and options used for OpenRGB patches with conjunction with VIA.

--- keyboards/keychron/k8_max/ansi/rgb/rgb.c
+++ keyboards/keychron/k8_max/ansi/rgb/rgb.c
@@ -16,6 +16,10 @@
 
 #include "quantum.h"
 
+#ifdef VIA_OPENRGB_HYBRID
+uint8_t is_orgb_mode;
+#endif
+
 // clang-format off
 #ifdef RGB_MATRIX_ENABLE
 const snled27351_led_t PROGMEM g_snled27351_leds[RGB_MATRIX_LED_COUNT] = {
